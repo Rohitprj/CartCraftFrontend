0376ddcc9147ca51a414a46c8559c6f3e975c092
Rohit kumar <rohitprj.1282@gmail.com>
2025-08-19 18:17:22 +0530

    project setup

--- src/utils/Constants.tsx
+++ src/utils/Constants.tsx
@@ -1,20 +1,21 @@
-import { Dimensions } from "react-native";
+import { Dimensions } from 'react-native';
 
-export const screenHeight = Dimensions.get('screen').height
-export const screenWidth = Dimensions.get('screen').width
+export const screenHeight = Dimensions.get('screen').height;
+export const screenWidth = Dimensions.get('screen').width;
 
 export enum FONTS {
-  heading = "CormorantGaramond-Medium",
-  heading2 = "CormorantGaramond-Regular",
+  heading = 'CormorantGaramond-Medium',
+  heading2 = 'CormorantGaramond-Regular',
 }
 
 export enum Colors {
   primary = '#FFC201',
   active = '#1054E8',
   inactive = '#666',
-  lightText = "#222",
+  lightText = '#222',
   background = '#fff',
   text = '#222',
+  extras = '#E49B0E',
 }
 
 export const formatDate = (dateString: string): string => {
@@ -26,4 +27,3 @@ export const formatDate = (dateString: string): string => {
 
   return `${day} ${month}, ${year}`;
 };
-
